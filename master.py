ac06391277786142ef461a4fad0d5aaf5772e5e7
nmashkov <nmashkov@gmail.com>
2024-10-10 16:22:05 +0300

    add columnCasts printing, various prefixes and selectors

--- master.py
+++ master.py
@@ -13,18 +13,22 @@ WORKING_DIR = f'{BASE_DIR}/{TARGET_DIR}'
 
 class App:
     def __init__(self):
-        # 
+        # MAIN
         self.dir_list = os.listdir(WORKING_DIR)
         self.mapping_dict = {}
         self.mapping_filename = ''
         self.main_df = ''
         self.enc = 'utf-8'
-        self.db_type = 0  # 1: Oracle, 2: MSSQL
+        self.db_type = 1  # 1: Oracle, 2: MSSQL
+        self.env_type = 2  # 1: Local, 2: Prod
+        self.flow_type_select = 2  # 1: columnCasts, 2: without columnCasts
         self.schtbl_json_max_cnt = 99
         # CBLOB
         self.TakeOnlyCBlobTables = 2  # 0: init select, 1: Yes, 2: No
-        self.IsCBlobTableIgnore = 1   # 0: init select, 1: Yes, 2: No
-        self.IsCBlobColumnIgnore = 2  # 0: init select, 1: Yes, 2: No
+        self.IsCBlobTableIgnore = 1   # all 0, or 1 and 2
+        self.IsCBlobColumnIgnore = 2  # default: 2 1 2
+        # COUNTS
+        self.source_counts_csv = ''
         # CHEATS
         self.custom_schema_s_name = ''
         self.ignore_table_s_list = []
@@ -42,6 +46,19 @@ class App:
         if self.db_type not in (1,2):
             print('=DB CHOOSE ERROR=')
             print(f"db_type: {self.db_type}")
+            self.pause()
+            exit()
+        
+        # select Environment type
+        if not self.env_type:
+            self.db_type = int(input(
+                '\nChoose Environment type:\n1: Local\n2: Prod\nYour choice: '
+            ))
+
+        # check Environment type
+        if self.db_type not in (1,2):
+            print('=ENV CHOOSE ERROR=')
+            print(f"env_type: {self.env_type}")
             self.pause()
             exit()
 
@@ -64,8 +81,14 @@ class App:
                     '\nIgnore CLOB/BLOB attributes for Oracle?:'
                     '\n1: Yes\n2: No\n'
                 ))
-        
 
+        # decide print columnCasts
+        if self.db_type == 1 and not self.flow_type_select:
+            self.flow_type_select = int(input(
+                    '\nPrint columnCasts in flows for Oracle?:'
+                    '\n1: Yes\n2: No\n'
+                ))
+        
     def pause(self):
         return input("\nPress the <ENTER> key to exit...")
 
@@ -114,7 +137,8 @@ class App:
                                             "J,"  # Source Length
                                             "T,"  # Target Schema
                                             "U,"  # Tagret Table
-                                            "V")  # Target Code
+                                            "V,"  # Target Code
+                                            "Z")  # Target Data Type
         except Exception as e:
             print("Error while reading file: ", e)
             self.pause()
@@ -122,8 +146,9 @@ class App:
         main_df = main_df.drop(0,axis=0)
 
         main_df.columns = ['SchemaS', 'TableS', 'CodeS',
-                           'Data Type', 'Length',
-                           'SchemaT', 'TableT', 'CodeT']
+                           'DataTypeS', 'Length',
+                           'SchemaT', 'TableT', 'CodeT',
+                           'DataTypeT']
         
         main_df = main_df.fillna('')
 
@@ -131,12 +156,14 @@ class App:
         main_df['SchemaS'] = main_df['SchemaS'].apply(lambda x: str(x).strip())
         main_df['TableS'] = main_df['TableS'].apply(lambda x: str(x).strip())
         main_df['CodeS'] = main_df['CodeS'].apply(lambda x: str(x).strip())
-        main_df['Data Type'] = main_df['Data Type']\
+        main_df['DataTypeS'] = main_df['DataTypeS']\
                                         .apply(lambda x: str(x).strip())
         main_df['Length'] = main_df['Length'].apply(lambda x: str(x).strip())
         main_df['SchemaT'] = main_df['SchemaT'].apply(lambda x: str(x).strip())
         main_df['TableT'] = main_df['TableT'].apply(lambda x: str(x).strip())
         main_df['CodeT'] = main_df['CodeT'].apply(lambda x: str(x).strip())
+        main_df['DataTypeT'] = main_df['DataTypeT']\
+                                        .apply(lambda x: str(x).strip())
 
         #
         main_df = main_df[main_df['CodeT']!='hdp_processed_dttm']
@@ -144,7 +171,7 @@ class App:
         # CBLOB
         if self.TakeOnlyCBlobTables == 1:
             cblob_table_df = main_df[
-                    main_df['Data Type'].isin(['CLOB', 'BLOB'])
+                    main_df['DataTypeS'].isin(['CLOB', 'BLOB'])
                 ]
             cblob_table_list = cblob_table_df['TableS'].unique().tolist()
             if cblob_table_list:
@@ -152,14 +179,14 @@ class App:
                                     .isin(cblob_table_list)]
         elif self.IsCBlobTableIgnore == 1:
             cblob_table_df = main_df[
-                    main_df['Data Type'].isin(['CLOB', 'BLOB'])
+                    main_df['DataTypeS'].isin(['CLOB', 'BLOB'])
                 ]
             ignore_cblob_table_list = cblob_table_df['TableS'].unique().tolist()
             if ignore_cblob_table_list:
                 main_df = main_df[~main_df['TableS']\
                                     .isin(ignore_cblob_table_list)]
         elif self.IsCBlobColumnIgnore == 1:
-            main_df = main_df[~main_df['Data Type'].isin(['CLOB', 'BLOB'])]
+            main_df = main_df[~main_df['DataTypeS'].isin(['CLOB', 'BLOB'])]
         
         # CHEAT
         if self.custom_schema_s_name:
@@ -226,19 +253,22 @@ class App:
                 query_suffix = ' from $schema.$table'
                 query_cast_list = []
 
+                columns_casts = []
+
                 for _, row in current_df.iterrows():
                     target_column_name = row['CodeT']
                     source_column_name = row['CodeS']
-                    source_column_type = row['Data Type']
+                    source_column_type = row['DataTypeS']
+                    target_column_type = row['DataTypeT']
                     source_column_length = ''
 
                     if row['Length'] and\
-                        row['Data Type'].lower() not in ('smallint',
+                        row['DataTypeS'].lower() not in ('smallint',
                                                          'date',
                                                          'int',
                                                          'integer'):
                         source_column_length = f"({row['Length']})"
-                    elif row['Data Type'].lower() == 'varchar2':
+                    elif row['DataTypeS'].lower() == 'varchar2':
                         source_column_length = '(255)'
                     else:
                         source_column_length = ''
@@ -249,22 +279,48 @@ class App:
                         f"'{target_column_name}'"
                         )
 
+                    columns_casts.append(
+                        {
+                            "name": target_column_name,
+                            "colType": target_column_type
+                        }
+                    )
+
                 query_full = ', '.join(query_cast_list)
 
                 query_full = query_prefix + query_full + query_suffix
 
-                flow_template = {
-                    "loadType": "Scd1Replace",
-                    "source": {
-                        "schema": schema_s,
-                        "table": source_table,
+                flow_template = {}
+
+                if self.flow_type_select == 1:
+                    # columnCasts
+                    flow_template = {
+                        "loadType": "Scd1Replace",
+                        "source": {
+                            "schema": schema_s,
+                            "table": source_table,
+                            "columnCasts": columns_casts,
+                            "jdbcDialect": "OracleDialect"
+                        },
                         "query": query_full,
-                        "jdbcDialect": "OracleDialect"
-                    },
-                    "target": {
-                        "table": target_table
+                        "target": {
+                            "table": target_table
+                        }
                     }
-                }
+                elif self.flow_type_select == 2:
+                    # without columnCasts
+                    flow_template = {
+                        "loadType": "Scd1Replace",
+                        "source": {
+                            "schema": schema_s,
+                            "table": source_table,
+                            "query": query_full,
+                            "jdbcDialect": "OracleDialect"
+                        },
+                        "target": {
+                            "table": target_table
+                        }
+                    }
                 
                 if schtbl_cnt_trigger < self.schtbl_json_max_cnt:
 
@@ -308,10 +364,10 @@ class App:
                 for _, row in current_df.iterrows():
                     attr_f = row['CodeT']
                     attr_l = row['CodeT']
-                    source_column_type = row['Data Type']
+                    source_column_type = row['DataTypeS']
                     source_column_length = ''
                     if row['Length'] and\
-                        row['Data Type'].lower() not in ('smallint',
+                        row['DataTypeS'].lower() not in ('smallint',
                                                         'date',
                                                         'int',
                                                         'integer'):
@@ -382,53 +438,57 @@ class App:
         # print result to file
         print('=PRINT RESULT=')
 
-        main_json_template = {
-            "connection": {
-                "connType": "jdbc",
-                "url": "jdbc:oracle:thin:@192.168.1.67:1521:FREE",
-                "driver": "oracle.jdbc.driver.OracleDriver",
-                "user": "ibank2",
-                "password": "ibank2"
-            },
-            "commonInfo": {
-                "targetSchema": schema_t,
-                "etlSchema": schema_t,
-                "logsTable": "logs556"
-            },
-            "flows": test_flow_entity_lst
-            }
+        main_json_template = {}
 
-        """
-        main_json_template_default = {
-            "connection": {
-                "connType": "jdbc",
-                "url": "...",
-                "driver": "...",
-                "user": "...",
-                "password": "..."
-            },
-            "commonInfo": {
-                "targetSchema": schema_t,
-                "etlSchema": schema_t,
-                "logsTable": "logs..."
-            },
-            "flows": test_flow_entity_lst
-            }
-        """
-        
-        prefix = """spark-submit \\
-                --master yarn \\
-                --conf spark.master=yarn \\
-                --conf spark.submit.deployMode=cluster \\
-                --conf spark.yarn.maxAppAttempts=1 \\
-                --conf spark.sql.broadcastTimeout=600 \\
-                --conf spark.hadoop.hive.exec.dynamic.partition=true \\
-                --conf spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict \\
-                --conf spark.driver.userClassPathFirst=true \\
-                --conf spark.executor.userClassPathFirst=true \\
-                --jars /home/hdoop/drivers/jcc-11.5.9.0.jar,/home/hdoop/drivers/commons-pool2-2.11.0.jar,/home/hdoop/drivers/delta-core_2.13-2.2.0.jar,/home/hdoop/drivers/delta-storage-2.2.0.jar,/home/hdoop/drivers/mssql-jdbc-9.2.1.jre8.jar,/home/hdoop/drivers/ojdbc8-21.6.0.0.1.jar,/home/hdoop/drivers/orai18n-19.3.0.0.jar,/home/hdoop/drivers/org.apache.servicemix.bundles.kafka-clients-2.4.1_1.jar,/home/hdoop/drivers/postgresql-42.3.1.jar,/home/hdoop/drivers/spark-sql-kafka-0-10_2.13-3.3.2.jar,/home/hdoop/drivers/spark-token-provider-kafka-0-10_2.13-3.3.2.jar,/home/hdoop/drivers/vertica-jdbc-11.1.0-0.jar,/home/hdoop/drivers/xdb6-18.3.0.0.jar,/home/hdoop/drivers/xmlparserv2-19.3.0.0.jar \\
-                --class sparketl.Main /home/hdoop/SparkEtl_ora.jar \\
-                ' """
+        # self.db_type = 1  # 1: Oracle, 2: MSSQL
+        if self.db_type == 1:
+            main_json_template = {
+                "connection": {
+                    "connType": "jdbc",
+                    "url": "TODO",
+                    "driver": "oracle.jdbc.driver.OracleDriver",
+                    "user": "TODO",
+                    "password": "TODO"
+                },
+                "commonInfo": {
+                    "targetSchema": schema_t,
+                    "etlSchema": schema_t,
+                    "logsTable": "TODO"
+                },
+                "flows": test_flow_entity_lst
+                }
+        elif self.db_type == 2:
+            main_json_template = {
+                "connection": {
+                    "connType": "jdbc",
+                    "url": "TODO",
+                    "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+                    "user": "TODO",
+                    "password": "TODO"
+                },
+                "commonInfo": {
+                    "targetSchema": schema_t,
+                    "etlSchema": schema_t,
+                    "logsTable": "TODO"
+                },
+                "flows": test_flow_entity_lst
+                }
+
+        prefix_local = """spark-submit \\\n--master yarn \\\n--conf spark.master=yarn \\\n--conf spark.submit.deployMode=cluster \\\n--conf spark.yarn.maxAppAttempts=1 \\\n--conf spark.sql.broadcastTimeout=600 \\\n--conf spark.hadoop.hive.exec.dynamic.partition=true \\\n--conf spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict \\\n--conf spark.driver.userClassPathFirst=true \\\n--conf spark.executor.userClassPathFirst=true \\\n--jars /home/hdoop/drivers/jcc-11.5.9.0.jar,/home/hdoop/drivers/commons-pool2-2.11.0.jar,/home/hdoop/drivers/delta-core_2.13-2.2.0.jar,/home/hdoop/drivers/delta-storage-2.2.0.jar,/home/hdoop/drivers/mssql-jdbc-9.2.1.jre8.jar,/home/hdoop/drivers/ojdbc8-21.6.0.0.1.jar,/home/hdoop/drivers/orai18n-19.3.0.0.jar,/home/hdoop/drivers/org.apache.servicemix.bundles.kafka-clients-2.4.1_1.jar,/home/hdoop/drivers/postgresql-42.3.1.jar,/home/hdoop/drivers/spark-sql-kafka-0-10_2.13-3.3.2.jar,/home/hdoop/drivers/spark-token-provider-kafka-0-10_2.13-3.3.2.jar,/home/hdoop/drivers/vertica-jdbc-11.1.0-0.jar,/home/hdoop/drivers/xdb6-18.3.0.0.jar,/home/hdoop/drivers/xmlparserv2-19.3.0.0.jar \\\n--class sparketl.Main /home/hdoop/SparkEtl_ora.jar \\\n' """
+
+        prefix_prod_ora = """
+        spark3-submit \\\n--keytab ~/TODO_TUZ_LD.keytab \\\n--principal TODO_TUZ_LD@REGION.VTB.RU \\\n--name TODO_SYSTEM_arch_TARGETNAME_load_N \\\n--master yarn \\\n--conf spark.master=yarn \\\n--conf spark.submit.deployMode=cluster \\\n--conf spark.dynamicAllocation.enabled=False \\\n--conf spark.driver.memory=3g \\\n--conf spark.executor.memory=2g \\\n--conf spark.executor.cores=4 \\\n--conf spark.executor.instances=6 \\\n--conf spark.executor.memoryOverhead=4g \\\n--conf spark.hadoop.hive.exec.dynamic.partition=True \\\n--conf spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict \\\n--conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY \\\n--conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY \\\n--conf spark.driver.extraJavaOptions=-Duser.timezone=Europe/Moscow \\\n--conf spark.executor.extraJavaOptions=-Duser.timezone=Europe/Moscow \\\n--jars hdfs:///apps/sparkjars/2239/ojdbc8.jar,hdfs:///apps/sparkjars/2239/orai18n.jar \\\n--class sparketl.Main \\\n--deploy-mode cluster hdfs:///apps/sparkjars/2239/SparkEtl.jar \\\n' """
+
+        prefix_prod_mssql = """spark3-submit \\\n--keytab ~/TODO_TUZ_LD.keytab \\\n--principal TODO_TUZ_LD@REGION.VTB.RU \\\n--name TODO_SYSTEM_arch_TARGETNAME_load_N \\\n--master yarn \\\n--conf spark.master=yarn \\\n--conf spark.submit.deployMode=cluster \\\n--conf spark.dynamicAllocation.enabled=False \\\n--conf spark.driver.memory=3g \\\n--conf spark.executor.memory=2g \\\n--conf spark.executor.cores=4 \\\n--conf spark.executor.instances=6 \\\n--conf spark.executor.memoryOverhead=4g \\\n--conf spark.hadoop.hive.exec.dynamic.partition=True \\\n--conf spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict \\\n--conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY \\\n--conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY \\\n--conf spark.driver.extraJavaOptions=-Duser.timezone=UTC \\\n--conf spark.executor.extraJavaOptions=-Duser.timezone=UTC \\\n--jars hdfs:///apps/sparkjars/2239/mssql-jdbc-9.2.1.jre8.jar \\\n--class sparketl.Main \\\n--deploy-mode cluster hdfs:///apps/sparkjars/2239/SparkEtl.jar \\\n' """
+
+        #
+        if self.env_type == 1:  # Local
+            prefix = prefix_local
+        elif self.env_type == 2:  # Prod
+            if self.db_type == 1:  # Oracle
+                prefix = prefix_prod_ora
+            elif self.db_type == 2:  # MSSQL
+                prefix = prefix_prod_mssql
 
         suffix = " '"
 
@@ -454,9 +514,25 @@ class App:
         else:
             blob_info = 'all_tables'
 
+        columnCasts_info = ''
+
+        if self.flow_type_select == 1:
+            columnCasts_info = 'columnCasts_'
+        else:
+            columnCasts_info = ''
+
+        env_info = ''
+
+        if self.env_type == 1:
+            env_info = 'local_'
+        elif self.env_type == 2:
+            env_info = 'prod_'
+
         results_dir = (
             f'{WORKING_DIR}/{results_file}_'
             f'{blob_info}_'
+            f'{columnCasts_info}'
+            f'{env_info}'
             f'{str(schtbl_num)}_{str(schtbl_len)}_'
             f'load.sh'
             )
